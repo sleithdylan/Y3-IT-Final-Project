5b8dd6bc4fda0fc2203211545a88f50575b77a1e
Dylan Sleith <sleithdylan@gmail.com>
2020-01-25 00:20:51 +0000

    Add home

--- index.php
+++ index.php
@@ -1,31 +1,230 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html class="no-js" lang="en">
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Intern. - Search for internships online to find the next step in your career.</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- CSS Dependencies -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://unpkg.com/shards-ui@latest/dist/css/shards.min.css">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  <script src="https://kit.fontawesome.com/1d6b99454b.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="assets/css/shards.min.css?v=3.0.0">
+  <link rel="stylesheet" href="assets/css/shards-extras.min.css?version=3.0.0">
 </head>
 
-<body>
-  <h1>Hello, world!</h1>
+<body class="shards-landing-page--1">
+  <!-- Welcome Section -->
+  <div class="welcome d-flex justify-content-center flex-column">
+    <div class="container">
+      <!-- Navigation -->
+      <nav class="navbar navbar-expand-lg navbar-dark pt-4 px-0">
+        <a class="navbar-brand" href="#">
+          <img src="assets/images/agency-landing/shards-logo-white.svg" class="mr-2" alt="Shards - Agency Landing Page">
+          Shards Agency
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">How It Works</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Internships</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Students
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Log in</a>
+                <a class="dropdown-item" href="#">Get Started</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Employers
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Log in</a>
+                <a class="dropdown-item" href="#">Get Started</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <!-- / Navigation -->
+    </div> <!-- .container -->
 
-  <!-- Optional JavaScript -->
-  <!-- JavaScript Dependencies: jQuery, Popper.js, Bootstrap JS, Shards JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <!-- Inner Wrapper -->
+    <div class="d-flex flex-column inner-wrapper mt-auto mb-auto container">
+      <div class="row">
+        <div class="col-5 mb-3">
+          <h1 class="welcome-heading h1 display-3 text-white">Intern.</h1>
+          <p class="h6 text-white">Search for internships online to find the next step in your career.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-5">
+          <div class="input-group input-group-seamless">
+            <input type="text" class="form-control" id="form2-password" placeholder="Search..">
+            <span class="input-group-append">
+              <span class="input-group-text">
+                <i class="fa fa-search"></i>
+              </span>
+            </span>
+          </div>
+        </div>
+        <div class="col-md">
+          <a href="#">
+            <button type="button" class="btn btn-outline-white">Find Internships</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- / Inner Wrapper -->
+  </div>
+  <!-- / Welcome Section -->
+
+  <!-- Our Services Section -->
+  <div id="our-services" class="our-services section py-4">
+    <h5 class="text-center mt-5">How It Works</h5>
+    <!-- Features -->
+    <div class="features py-5">
+      <div class="container">
+        <div class="row">
+          <div class="feature py-4 col-md-4 d-flex flex-column justify-content-center align-items-center">
+            <div class="icon text-primary"><i class="fa fa-binoculars"></i></div>
+            <div class="px-3 py-4">
+              <h5 class="text-center">Find Internship</h5>
+              <p class="text-center">Pellentesque sit amet libero vitae nibh imperdiet dictum at tempor ipsum.</p>
+            </div>
+          </div>
+          <div class="feature py-4 col-md-4 d-flex flex-column justify-content-center align-items-center">
+            <div class="icon text-primary"><i class="fas fa-clipboard-check"></i></div>
+            <div class="px-3 py-4">
+              <h5 class="text-center">Apply</h5>
+              <p class="text-center">Mauris iaculis tristique lacus eu tincidunt. Duis ac leo vel est dictum consequat
+              </p>
+            </div>
+          </div>
+          <div class="feature py-4 col-md-4 d-flex flex-column justify-content-center align-items-center">
+            <div class="icon text-primary"> <i class="fas fa-handshake"></i></div>
+            <div class="px-3 py-4">
+              <h5 class="text-center">Get Hired</h5>
+              <p class="text-center">Donec tristique nisl et molestie aliquet. Praesent sit amet mauris porta augue
+                congue.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / Features -->
+  </div>
+  <!-- / Our Services Section -->
+
+  <!-- Our Blog Section -->
+  <div class="blog section section-invert py-4">
+    <h5 class="text-center mt-5">Get Started</h5>
+
+    <div class="container text-center mb-5">
+      <div class="py-5">
+        <div class="row">
+          <div class="col-md-7 mx-auto">
+            <h3 class="welcome-heading h3 mb-3">Start searching for internships for free</h3>
+            <p>Duis aliquam aliquam pharetra. Aenean volutpat felis dolor, ac vestibulum nunc fermentum
+              at. Ut egestas auctor sapien eget bibendum. Vestibulum id rhoncus eros. Suspendisse ut urna augue.
+            </p>
+            <a href="#">
+              <button type="button" class="btn btn-primary">Get Started</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- / Our Blog Section -->
+
+  <!-- Footer Section -->
+  <footer>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-md-3">
+            <a class="navbar-brand" href="#">Intern.</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div class="col-md-3">
+            <ul class="navbar-nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul class="navbar-nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Active</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul class="navbar-nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Active</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </footer>
+  <!-- / Footer Section -->
+
+  <!-- JavaScript Dependencies -->
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+    integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
   </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
   </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
-  </script>
-  <script src="https://unpkg.com/shards-ui@latest/dist/js/shards.min.js"></script>
 </body>
 
 </html>
