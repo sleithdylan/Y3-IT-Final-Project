82064946f488bf56f41ebdad4ae7de7a60d1599d
Dylan Sleith <sleithdylan@gmail.com>
2020-04-03 21:14:14 +0100

    Setup Heroku db

--- config/config.php
+++ config/config.php
@@ -5,4 +5,10 @@ $DB_USER = 'root';
 $DB_PASS = '';
 $DB_NAME = 'studenthired';
 
+// Heroku's DB
+// $DB_HOST = 'eu-cdbr-west-02.cleardb.net';
+// $DB_USER = 'bd76598f663295';
+// $DB_PASS = '65939272';
+// $DB_NAME = 'heroku_b4fe9eafa1054b2';
+
 ?>
