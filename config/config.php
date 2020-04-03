16b71e0675ebd48617776719619679f20744f404
Dylan Sleith <sleithdylan@gmail.com>
2020-04-03 21:15:39 +0100

    Update config

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 // Initialize Variables
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = '';
-$DB_NAME = 'studenthired';
+// $DB_HOST = 'localhost';
+// $DB_USER = 'root';
+// $DB_PASS = '';
+// $DB_NAME = 'studenthired';
 
 // Heroku's DB
-// $DB_HOST = 'eu-cdbr-west-02.cleardb.net';
-// $DB_USER = 'bd76598f663295';
-// $DB_PASS = '65939272';
-// $DB_NAME = 'heroku_b4fe9eafa1054b2';
+$DB_HOST = 'eu-cdbr-west-02.cleardb.net';
+$DB_USER = 'bd76598f663295';
+$DB_PASS = '65939272';
+$DB_NAME = 'heroku_b4fe9eafa1054b2';
 
 ?>
